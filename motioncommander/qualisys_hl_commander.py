4fad8e4e2de6b3b33b5d7150c992229ab152309b
Shreeram Murali <45518087+shreeram-murali@users.noreply.github.com>
2022-01-03 12:10:10 +0530

    Update

--- motioncommander/qualisys_hl_commander.py
+++ motioncommander/qualisys_hl_commander.py
@@ -1,24 +1,3 @@
-# -*- coding: utf-8 -*-
-#
-# ,---------,       ____  _ __
-# |  ,-^-,  |      / __ )(_) /_______________ _____  ___
-# | (  O  ) |     / __  / / __/ ___/ ___/ __ `/_  / / _ \
-# | / ,--'  |    / /_/ / / /_/ /__/ /  / /_/ / / /_/  __/
-#    +------`   /_____/_/\__/\___/_/   \__,_/ /___/\___/
-#
-# Copyright (C) 2019 Bitcraze AB
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, in version 3.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program. If not, see <http://www.gnu.org/licenses/>.
 """
 Example of how to connect to a Qualisys QTM system and feed the position to a
 Crazyflie. It uses the high level commander to upload a trajectory to fly a
